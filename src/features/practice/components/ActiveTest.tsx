afc2fb0c7bbd006f840835eac04df95081c3646b
Vivek <vivek@vivekmind.com>
2026-04-23 18:16:18 +0530

    fixed

--- src/features/practice/components/ActiveTest.tsx
+++ src/features/practice/components/ActiveTest.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Timer, ArrowLeft, ArrowRight } from "lucide-react";
